fe390f06a451adf9290821982e6191bb71ce84fb
0xPh4nt0m <0xPh4nt0m@protonmail.com>
2020-01-30 21:36:48 -0800

    dynamically add the game

--- js/main.js
+++ js/main.js
@@ -1,3 +1,36 @@
-console.log("test")
-let newtest = "test";
-document.getElementsByClassName("container")[0].innerHTML = "<h1>TEST</h1>"
+// Dynmaically load the sites games
+const Games = document.getElementsByClassName("games")[0];
+const gamelist = document.getElementById("gamelist");
+
+$("option").on("click", () => {
+
+    let ChosenGame = gamelist.options;
+    let ChosenGameIndex = gamelist.selectedIndex;
+    switch (ChosenGameIndex) {
+        case 0:
+            Games.innerHTML = `<embed src="./games/${ChosenGame[ChosenGameIndex].value}.swf" width="550" height="400"> </embed>`
+            break;
+        case 1:
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            break;
+        case 5:
+            break;
+        case 6:
+            break;
+        case 7:
+            break;
+        case 8:
+            break;
+        case 9:
+            break;
+        default:
+            console.log("no game")
+
+    }
+
+})
