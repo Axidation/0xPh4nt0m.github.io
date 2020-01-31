86ad816d0f7c0eaf4f91272c1a285f76d3ba1f4f
0xPh4nt0m <0xPh4nt0m@protonmail.com>
2020-01-30 22:23:10 -0800

    dynamically add any game listed in options

--- js/main.js
+++ js/main.js
@@ -1,39 +1,19 @@
 // Dynmaically load the sites games
 const Games = document.getElementsByClassName("games")[0];
 const gamelist = document.getElementById("gamelist");
-
+const gameListLength = gamelist.length;
 $("#gamelist").on("change", () => {
 
     let ChosenGame = gamelist.options;
     let ChosenGameIndex = gamelist.selectedIndex;
-    switch (ChosenGameIndex) {
-        case 0:
-            Games.innerHTML = `<embed src="./games/${ChosenGame[ChosenGameIndex].value}.swf" width="550" height="400"> </embed>`
-            console.log("test")
-            break;
-        case 1:
-            console.log("test 1")
-            break;
-        case 2:
-            console.log("test2 ")
-            break;
-        case 3:
-            break;
-        case 4:
-            break;
-        case 5:
-            break;
-        case 6:
-            break;
-        case 7:
-            break;
-        case 8:
-            break;
-        case 9:
-            break;
-        default:
-            console.log("no game")
 
+    for (i = 0; i < gameListLength; i++) {
+        switch (ChosenGameIndex) {
+            case i:
+                Games.innerHTML = `<embed src="./games/${ChosenGame[ChosenGameIndex].value}.swf" width="720" height="400"> </embed>`
+                break;
+            default:
+                console.log("no game")
+        }
     }
-
 })
